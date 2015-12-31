840bbf63387a8169ae3fcf559b35114f53a8de73
Marcelo Pereira <mjrpereira@yahoo.com.br>
2015-12-31 20:04:17 -0200

    Cors Funcionando.

--- vitrine/src/filter/ResponseFilter.java
+++ vitrine/src/filter/ResponseFilter.java
@@ -8,10 +8,13 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response;
 
 public class ResponseFilter implements Filter {
 
+	@Override	
 	public void init(FilterConfig filterConfig) throws ServletException
 	{
 
@@ -20,15 +23,19 @@ public class ResponseFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain filterChain) throws IOException, ServletException {
-
-		HttpServletResponse httpResponse = (HttpServletResponse)response;
+		
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
 		httpResponse.setHeader("Access-Control-Allow-Origin", "*");
 		httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEADER");
 		httpResponse.setHeader("Access-Control-Allow-Headers",
 				"X-Requested-With, Host,User-Agent, Accept, Accept-Language, " +
 				"Accept-Encoding, Accept-Charset, Keep-Alive, Connection, Referer, Origin, Content-Type");
 		
+		System.out.println(httpResponse.getHeaderNames().toString());
+		
+
 		filterChain.doFilter(request, response);
+		
 	}
 
 	@Override
